1304ab461d6b274b942ec104b8c0f047bf96696a
pukar756 <pukar756@gmail.com>
2024-01-14 16:17:07 +0545

    commiting V1 and V2 tf file

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+region  = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-0005e0cfe09cc9050"
     instance_type = "t2.micro"
     key_name = "dpp"
+  
 }
